26e4fd6dc1135605f4100a1ef4d1044ed892c40d
llevasse <elise.levasse@gmail.com>
2024-06-30 17:07:58 +0200

    [front][bug fix]dont add first page loaded to the browser history twice

--- src/webserver/html/app.js
+++ src/webserver/html/app.js
@@ -1,16 +1,19 @@
-loadPage('login');
+loadPage('login', false);
 
-
-async function loadPage(wanted){
+async function loadPage(wanted, save){
 	const contain = document.getElementById("container");
 	const response = await fetch(`bodyLess/${wanted}.html`);
 	const txt = await response.text();
-	history.pushState(txt, "");
 	contain.innerHTML=txt;
-
+	console.log(`addstate ${save}: ${txt}`);
+	if (save == true)
+		history.pushState(txt, "");
+	else
+		history.replaceState(txt,"");
 }
 
 window.addEventListener("popstate", (event) => {
+	console.log(`popstate: ${event.state}`);
 	if (event.state){
 		const contain = document.getElementById("container");
 		contain.innerHTML = event.state;
