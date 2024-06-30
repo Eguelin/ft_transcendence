edb2dd30be5943121e3bcca443e1286ec6912996
naterrie <naterrie@student.42lyon.fr>
2024-06-30 17:01:46 +0200

    feat: Adding path for login, current, create and logout from user

--- src/webserver/html/app.js
+++ src/webserver/html/app.js
@@ -4,7 +4,7 @@ async function changeToLogin(){
 	const contain = document.getElementById("container");
 	const response = await fetch("bodyLess/login.html");
 	const txt = await response.text();
-	
+
 	contain.innerHTML=txt;
 }
 
@@ -12,7 +12,7 @@ async function changeToRegister(){
 	const contain = document.getElementById("container");
 	const response = await fetch("bodyLess/register.html");
 	const txt = await response.text();
-	
+
 	contain.innerHTML=txt;
 }
 
@@ -20,7 +20,7 @@ async function changeToSettings(){
 	const contain = document.getElementById("container");
 	const response = await fetch("bodyLess/settings.html");
 	const txt = await response.text();
-	
+
 	contain.innerHTML=txt;
 }
 
@@ -28,7 +28,7 @@ async function changeToHome(){
 	const contain = document.getElementById("container");
 	const response = await fetch("bodyLess/home.html");
 	const txt = await response.text();
-	
+
 	contain.innerHTML=txt;
 }
 
@@ -49,4 +49,76 @@ async function changeToDarkMode(){
 async function changeToLightMode(){
 	const style = document.getElementById("style");
 	style.setAttribute('href', "lightMode.css");
+}
+
+function loginUser(username, password)
+{
+	const data = {username: username, password: password};
+	fetch('/api/user/login', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(data),
+		credentials: 'include'
+	})
+	.then(response => {
+		if (response.ok) {
+			console.log('User logged in successfully');
+			changeToHome();
+		} else {
+			console.log("Failed to login user")
+		}
+	})
+	.catch(error => {
+		console.error('There was a problem with the fetch operation:', error);
+	});
+}
+
+function createUser(username, password)
+{
+	const data = {username: username, password: password};
+	fetch('/api/user/create', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		credentials: 'include',
+		body: JSON.stringify(data)
+	})
+	.then(response => {
+		if (response.ok) {
+			console.log('User created successfully');
+			changeToLogin();
+		} else {
+			console.log("Failed to create user")
+		}
+	})
+	.catch(error => {
+		console.error('There was a problem with the fetch operation:', error);
+	});
+}
+
+function getCurrentUser() {
+	fetch('/api/user/current', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		credentials: 'include'
+	})
+	.then(response => {
+		if (response.ok) {
+			return response.json();
+		}
+		console.log("Failed to get user")
+	})
+	.then(data => {
+		console.log('User ID:', data.id);
+		console.log('Username:', data.username);
+		console.log('Email:', data.email);
+	})
+	.catch(error => {
+		console.error('There was a problem with the fetch operation:', error);
+	});
 }
