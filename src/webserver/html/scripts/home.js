376da2fa904a8e7b86cdc5432bb8865fedaf24da
llevasse <elise.levasse@gmail.com>
2024-07-07 21:46:28 +0200

    [feat] replaced most functions my eventListener

--- src/webserver/html/scripts/home.js
+++ src/webserver/html/scripts/home.js
@@ -1,166 +1,45 @@
-loadPage('login');
-//loadPage('register');
+container = document.getElementById("container");
+settingsBtn = document.getElementById("settingsBtn");
+logOutBtn = document.getElementById('logOutBtn');
+swichTheme = document.getElementById("themeButton");
 
-async function loadPage(wanted){
-	const contain = document.getElementById("container");
-	const response = await fetch(`bodyLess/${wanted}.html`);
-	const txt = await response.text();
-	if (contain.innerHTML != "")
-		history.pushState(txt, "");
-	else
-		history.replaceState(txt,"");
-	contain.innerHTML=txt;
-}
+settingsBtn.addEventListener("click", (e) => {
+	fetch ('bodyLess/settings.html').then((response) => {
+		return (response.text().then(response => {
+			if (container.innerHTML != "")
+				history.pushState(response, "");
+			else
+				history.replaceState(response,"");
+			container.innerHTML = response;
+			document.getElementById("script").remove();
+			var s = document.createElement("script");
+			s.setAttribute('id', 'script');
+			s.setAttribute('src', `scripts/settings.js`);
+			document.body.appendChild(s);
+		}))
+	});
+})
 
-window.addEventListener("popstate", (event) => {
-	if (event.state){
-		const contain = document.getElementById("container");
-		contain.innerHTML = event.state;
-	}
-});
+logOutBtn.addEventListener("click", (e) => {
+	fetch ('bodyLess/login.html').then((response) => {
+		return (response.text().then(response => {
+			if (container.innerHTML != "")
+				history.pushState(response, "");
+			else
+				history.replaceState(response,"");
+			container.innerHTML = response;
+			document.getElementById("script").remove();
+			var s = document.createElement("script");
+			s.setAttribute('id', 'script');
+			s.setAttribute('src', `scripts/login.js`);
+			document.body.appendChild(s);
+		}))
+	});
+})
 
-async function switchTheme(){
+
+swichTheme.addEventListener("click", () => {
 	const style = document.getElementById("style");
 	const href = style.getAttribute('href');
 	style.setAttribute('href', href == "lightMode.css" ? "darkMode.css" : "lightMode.css");
-}
-
-async function loadTheme(wanted){
-	const style = document.getElementById("style");
-	style.setAttribute('href', `${wanted}Mode.css`);
-}
-
-function login(){
-	username = document.getElementById('username').value;
-	pw = document.getElementById('password').value;
-	inputs = document.getElementsByClassName('formInput');
-	warning = document.createElement("a");
-	warning.className = "warning";
-	warning.text = "Field can't be empty";
-	btn = document.getElementById('loginBtn');
-	if (btn.previousElementSibling)
-		btn.previousElementSibling.remove();
-	for (i=0;i<inputs.length;i++){
-		if (inputs[i].value == "" && !inputs[i].previousElementSibling){
-			warningTmp = warning.cloneNode(true);
-			inputs[i].before(warningTmp);
-		}
-		if (inputs[i].value != "" && inputs[i].previousElementSibling){
-			inputs[i].previousElementSibling.remove();
-		}
-	}
-	if (username != "" && pw != "")
-		loginUser(username, pw);
-}
-
-async function loginUser(username, password)
-{
-	const data = {username: username, password: password};
-	let txt = await fetch('/api/user/login', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify(data),
-		credentials: 'include'
-	})
-	.then(response => {
-		if (response.ok) {
-			console.log('User logged in successfully');
-			loadPage('home');
-			return ("");
-		} else {
-			console.log("Failed to login user")
-			return response.json().then((text => {
-				return (text.message);
-			}));
-		}
-	})
-	warning = (document.createElement("a"));
-	warning.className = "warning";
-	warning.text = txt;
-	btn = document.getElementById('loginBtn');
-	if (!btn.previousElementSibling)
-		btn.before(warning);
-}
-
-function registerUser(){
-	email = document.getElementById('mail').value;
-	var lock = 0;
-	username = document.getElementById('username').value;
-	pw = document.getElementById('password').value;
-	cpw = document.getElementById('cPassword').value;
-	inputs = document.getElementsByClassName('formInput');
-	warning = document.createElement("a");
-	warning.className = "warning";
-	warning.text = "Field can't be empty";
-	for (i=0;i<inputs.length;i++){
-		if (inputs[i].previousElementSibling)
-			inputs[i].previousElementSibling.remove();
-		if (inputs[i].value == "" && !inputs[i].previousElementSibling){
-			warningTmp = warning.cloneNode(true);
-			inputs[i].before(warningTmp);
-			lock = 1;
-		}
-	}
-	
-	if (pw != cpw){
-		warning = document.createElement("a");
-		warning.className = "warning";
-		warning.text = "Passwords do not match";
-		if (document.getElementById('cPassword').previousElementSibling && document.getElementById('cPassword').previousElementSibling.text == "Field can't be empty"){
-			document.getElementById('cPassword').previousElementSibling.remove();
-		}
-		if (!document.getElementById('cPassword').previousElementSibling || document.getElementById('cPassword').previousElementSibling.text != "Passwords do not match"){
-			document.getElementById("cPassword").before(warning);
-		}
-		else if (cpw != "" && document.getElementById('cPassword').previousElementSibling.text == "Field can't be empty"){
-			document.getElementById('cPassword').previousElementSibling.remove();
-		}
-	}
-	else if (lock == 0)
-		createUser(username, pw);
-}
-
-function createUser(username, password)
-{
-	const data = {username: username, password: password};
-	fetch('/api/user/create', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		credentials: 'include',
-		body: JSON.stringify(data)
-	})
-	.then(response => {
-		if (response.ok) {
-			console.log('User created successfully');
-			loadPage('home');
-		} else {
-			console.log("Failed to create user")
-		}
-	})
-	.catch(error => {
-		console.error('There was a problem with the fetch operation:', error);
-	});
-}
-
-function getCurrentUser() {
-	fetch('/api/user/current', {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		credentials: 'include'
-	})
-	.then(response => {
-		if (response.ok) {
-			return response.json();
-		}
-		console.log("Failed to get user")
-	})
-	.catch(error => {
-		console.error('There was a problem with the fetch operation:', error);
-	});
-}
+})
