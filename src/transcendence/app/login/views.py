ca4ba84085cd088b76b64e74bf18a26c3d4b5457
llevasse <elise.levasse@gmail.com>
2024-07-11 14:47:36 +0200

    [feat] create User Profile model

--- src/transcendence/app/login/views.py
+++ src/transcendence/app/login/views.py
@@ -58,6 +58,6 @@ def current_user(request):
 	if request.method != 'GET':
 		return JsonResponse({'message': 'Invalid request'}, status=400)
 	if request.user.is_authenticated:
-		return JsonResponse({'username': request.user.username})
+		return JsonResponse({'username': request.user.username, 'theme': request.user.profile.dark_theme})
 	else:
 		return JsonResponse({'username': None}, status=400)
