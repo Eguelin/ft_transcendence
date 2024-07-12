9e5dd3f1cd02d6e5f3287148d51fe506d3f4638c
llevasse <elise.levasse@gmail.com>
2024-07-12 23:59:47 +0200

    [feat] added default pfp path in User.Profile and can display it on front end

--- src/transcendence/app/transcendence/settings.py
+++ src/transcendence/app/transcendence/settings.py
@@ -125,4 +125,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-MEDIA_ROOT = 'var/lib/postgresql/media'
+MEDIA_ROOT = 'profilePicture'
+URL_ROOT = 'profilePicture'
