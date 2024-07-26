8866c2e9d133d561f6fa70bee1064e373e515e27
Celeste <naterrie@student.42lyon.fr>
2024-07-26 14:07:28 +0200

    start id users for 42

--- src/transcendence/app/login/models.py
+++ src/transcendence/app/login/models.py
@@ -17,12 +17,13 @@ class Profile(models.Model):
 	profile_picture = models.TextField(default="profilePictures/defaults/default0.jpg")
 	language_pack = models.CharField(max_length=40, default="lang/EN_US.json")
 	display_name = models.CharField(max_length=15)
-	
+	id = models.CharField(default=0, primary_key=True)
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwards):
 	if created:
 		Profile.objects.create(user=instance)
-		
+
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
 	instance.profile.save()
