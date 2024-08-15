84c0f110a8b24bf6ef20496bf3025a8ab9acddd2
llevasse <elise.levasse@gmail.com>
2024-08-15 15:47:08 +0200

    [front] set notification display to none when all friend request are accepted or rejected

--- src/webserver/html/scripts/friends.js
+++ src/webserver/html/scripts/friends.js
@@ -140,7 +140,6 @@ function checkUpdate(){
 					});
 					var friends_request = text.friend_request;
 					notificationDot = document.getElementById("notificationDot");
-					console.log(typeof(friends_request));
 					notificationDot.style.setProperty("display", Object.keys(friends_request).length > 0 ? "block" : "none");
 					Object.keys(friends_request).forEach(function(key) {
 						friendContainer = document.createElement("div");
@@ -213,6 +212,7 @@ function checkUpdate(){
 							e.srcElement.parentElement.remove();
 							if (friendRequestPopup.innerHTML == ""){
 								friendRequestPopup.style.setProperty("display", "none");
+								notificationDot.style.setProperty("display", "none");
 								var bg = document.getElementById("popupBg");
 								if (bg != null)
 									bg.remove();
@@ -231,6 +231,7 @@ function checkUpdate(){
 							e.srcElement.parentElement.remove();
 							if (friendRequestPopup.innerHTML == ""){
 								friendRequestPopup.style.setProperty("display", "none");
+								notificationDot.style.setProperty("display", "none");
 								var bg = document.getElementById("popupBg");
 								if (bg != null)
 									bg.remove();
