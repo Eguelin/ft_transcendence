f4ead96b3b5580c2d56fc50aa7cd4a994591ed04
llevasse <elise.levasse@gmail.com>
2024-08-29 22:14:43 +0200

    [font] display user win/loss ratio, total number of games played, number of wins and of loses on the user profile page

--- src/webserver/html/scripts/app.js
+++ src/webserver/html/scripts/app.js
@@ -59,9 +59,24 @@ window.navigation.addEventListener("navigate", (e) => {
 											else
 												document.getElementById("profilePfp").style.setProperty("display", "none");
 											recentMatchHistoryContainer = document.getElementById("recentMatchHistoryContainer");
+											var countWin = 0, countLost = 0;
 											for (var i=0; i<Object.keys(user.matches).length && i<5;i++){
 												createMatchResumeContainer(user.matches[i]);
 											};
+											for (var i=0; i<Object.keys(user.matches).length;i++){
+												if (user.matches[i].player_one == user.display){
+													countWin += user.matches[i].player_one_pts > user.matches[i].player_two_pts;
+													countLost += user.matches[i].player_one_pts < user.matches[i].player_two_pts;
+												}
+												else{
+													countWin += user.matches[i].player_one_pts < user.matches[i].player_two_pts;
+													countLost += user.matches[i].player_one_pts > user.matches[i].player_two_pts;
+												}
+											}
+											document.getElementById("ratioContainer").innerHTML += `${countWin / Object.keys(user.matches).length}%`
+											document.getElementById("nbWinContainer").innerHTML += `${countWin}`
+											document.getElementById("nbLossContainer").innerHTML += `${countLost}`
+											document.getElementById("nbMatchContainer").innerHTML += `${Object.keys(user.matches).length}`
 											matchUsersName = document.querySelectorAll(".resultScoreName")
 											Object.keys(matchUsersName).forEach(function(key){
 												matchUsersName[key].addEventListener("click", (e) => {
