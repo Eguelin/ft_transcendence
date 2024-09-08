0cf59a53def28668fe540aa19078cdea6f26f572
llevasse <elise.levasse@gmail.com>
2024-09-08 16:50:17 +0200

    [fix] could'nt send friend request

--- src/nginx/html/scripts/profile.js
+++ src/nginx/html/scripts/profile.js
@@ -1,14 +1,14 @@
+var splitPath = window.location.href.split('/');
 sendFriendRequestBtn = document.getElementById("sendFriendRequestBtn");
 
 if (sendFriendRequestBtn){
     sendFriendRequestBtn.addEventListener("click", (e) => {
-        const data = {username: sendFriendRequestBtn.className};
         fetch('/api/user/send_friend_request', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(data),
+            body: JSON.stringify({'username': splitPath[4]}),
             credentials: 'include'
         })
     })   
