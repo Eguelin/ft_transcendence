cc94d292280d3623d9cc949722638751c737babe
Eguelin <eguelin@student.42lyon.fr>
2024-09-23 18:27:26 +0200

    feat: Add send_message and send_message_to_group methods to GameConsumer

--- src/nginx/html/scripts/game.js
+++ src/nginx/html/scripts/game.js
@@ -1,18 +1,36 @@
-const canvas = document.getElementById('game');
-const ctx = canvas.getContext('2d');
+function game() {
+	const canvas = document.getElementById('game');
+	const ctx = canvas.getContext('2d');
 
-var socket = new WebSocket("/ws/game/");
+	var socket = new WebSocket("/ws/game/");
 
-socket.onopen = function() {
-	console.log("Connection established");
-	socket.send(JSON.stringify({type: "connect", message: "Hello"}));
+	socket.onopen = function() {
+		console.log("Connection established");
+	}
+
+	socket.onmessage = function(event) {
+		var data = JSON.parse(event.data);
+		console.log(data);
+	}
+
+	socket.onclose = function() {
+		console.log("Connection closed");
+	}
+
+	// Close the socket when the user leaves the page
+	window.addEventListener('beforeunload', function() {
+		socket.close();
+	});
+
+	// Close the socket when the user clicks the "goHomeButton"
+	document.getElementById('goHomeButton').addEventListener('click', function() {
+		socket.close();
+	});
+
+	// Close the socket when the user uses the browser's back button
+	window.addEventListener('popstate', function() {
+		socket.close();
+	});
 }
 
-socket.onmessage = function(event) {
-	var data = JSON.parse(event.data);
-	console.log(data);
-}
-
-socket.onclose = function() {
-	console.log("Connection closed");
-}
+game();
