9a7946611019597097ea85174dded7c1730dfc92
llevasse <elise.levasse@gmail.com>
2024-09-28 21:32:38 +0200

    [fix] every pfp (excepect the last one of each list) not being resized

--- src/django/app/login/views.py
+++ src/django/app/login/views.py
@@ -139,7 +139,7 @@ def create_user(request):
 			user.profile.language_pack = data['lang']
 
 		# CREATE RANDOM FIRST MATCH
-		for i in range(0, 100):
+		for i in range(0, 10):
 			match = customModels.Match.objects.createWithRandomOpps(user)
 			user.profile.matches.add(match)
 		user = authenticate(request, username=username, password=password)
