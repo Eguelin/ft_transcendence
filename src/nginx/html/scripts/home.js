6b01bf6b9cfae520431ebd3312e8b5ed27b34e3a
llevasse <elise.levasse@gmail.com>
2024-09-29 13:14:58 +0200

    [front] added message in recent match history on home page if user has not yet played today

--- src/nginx/html/scripts/home.js
+++ src/nginx/html/scripts/home.js
@@ -21,10 +21,20 @@ playBtn = document.getElementById("playBtn");
 		else{
 			response.json().then(currentUser => {
 				matches = currentUser.matches;
-				recentMatchHistoryContainer.innerHTML = "";
-				for (var i=0; i<Object.keys(matches).length && i<5;i++){
-					createMatchResumeContainer(matches[i]);
-				};
+				if (Object.keys(matches).length == 0){
+					var message = document.createElement("a");
+					recentMatchHistoryContainer.style.setProperty("background", "var(--input-bg-rgb)");
+					recentMatchHistoryContainer.style.setProperty("align-items", "center");
+					message.style.setProperty("color", "var(--main-text-rgb)");	
+					message.style.setProperty("width", "100vw");
+					message.innerText = "You have not played any match today";
+					recentMatchHistoryContainer.appendChild(message);
+				}
+				else{
+					recentMatchHistoryContainer.innerHTML = "";
+					for (var i=0; i<Object.keys(matches).length && i<5;i++)
+						createMatchResumeContainer(matches[i]);
+				}
 				matchUsersName = document.querySelectorAll(".resultScoreName")
 				Object.keys(matchUsersName).forEach(function(key){
 					matchUsersName[key].addEventListener("click", (e) => {
