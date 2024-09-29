0404535bdc56616a7af779c116757ca6bd92bef7
llevasse <elise.levasse@gmail.com>
2024-09-29 21:42:22 +0200

    [feat] added create user fields in admin page

--- src/nginx/html/scripts/admin.js
+++ src/nginx/html/scripts/admin.js
@@ -1,3 +1,14 @@
-{
-    
-}
+createUserBtn = document.getElementById("createUserBtn");
+createUserBtn.addEventListener("click", (e) => {
+    username = document.getElementById("createUserUsername").value;
+    password = document.getElementById("createUserPassword").value;
+
+    fetch('/api/user/admin_create', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+        body: JSON.stringify({username: username, password: password})
+    })
+})
