c2ad34ca7ce68779d72f959b70d5f52448f0620f
llevasse <elise.levasse@gmail.com>
2024-10-07 16:35:44 +0200

    [front] slide selector are now clickable

--- src/nginx/html/scripts/login.js
+++ src/nginx/html/scripts/login.js
@@ -11,6 +11,19 @@ for (i = 0; i < slides.length; i++)
 slides[slideIdx].style.display = "block";
 
 
+Object.keys(loginSlideSelector).forEach(function(key) {
+	if (currentPage == "login"){
+		loginSlideSelector[key].addEventListener("click", (e) => {
+			loginSlideSelector[slideIdx].classList.remove("activeSelector");
+			slideIdx = Array.from(e.target.parentElement.children).indexOf(e.target);
+			for (i = 0; i < slides.length; i++) 
+				slides[i].style.display = "none";
+			slides[slideIdx].style.display = "block";
+			loginSlideSelector[slideIdx].classList.add('activeSelector');
+		})
+	}
+})
+
 fortyTwoLogin.addEventListener("click", (e) => {
 	const authUrl = `https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-cb9676bf45bf8955cbb6ab78a74a365e69a9f11a901301c48e5f5f5ee1a7c144&redirect_uri=https%3A%2F%2Flocalhost%3A49300%2F&response_type=code`;
 	window.location.href = authUrl;
