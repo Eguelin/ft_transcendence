77f5027afaf53b961cb1db8ecce8e873aeafbb3e
llevasse <elise.levasse@gmail.com>
2024-11-09 16:23:03 +0100

    [front] rework search feature to be more easy to use on mobile

--- src/nginx/html/scripts/search.js
+++ src/nginx/html/scripts/search.js
@@ -1,74 +1,88 @@
 var template = `
 <div id="pageContentContainer">
-    <div id="userResumeCountContainer">
-        <a id="userResumeCount"></a>
-        <a id="userResumeCountText">results matched</a>
-        <a id="userResumeSearch"></a>
-    </div>
-    <div id="resumeContainer">
 
-    </div>
+	<div id="userSearchContainer">
+		<input tabindex="12" id="inputSearch" placeholder="Search user" aria-label="Search user input field">
+		<div tabindex="13" id="searchBtn"></div>
+	</div>
+	<div id="resumeContainer">
+		<div id="userResumeCountContainer">
+			<a id="userResumeCount"></a>
+			<a id="userResumeCountText">results matched</a>
+			<a id="userResumeSearch"></a>
+		</div>
+	</div>
+
 </div>`
 
 {
-    document.getElementById("container").innerHTML = template;
-    inputSearchUserContainer.style.setProperty("display", "block");
-	document.getElementById("inputSearchUser").focus();
+	document.getElementById("container").innerHTML = template;
+	inputSearchUserContainer.style.setProperty("display", "none");
 	dropDownUserContainer.style.setProperty("display", "none");
 	homeBtn.style.setProperty("display", "block");
 	notifCenterContainer.style.setProperty("display", "flex");
 
-    url = new URL(window.location.href);
-    if (url.searchParams.get("query")) {
-        fetch('/api/user/search_by_username', {
-            method: 'POST', //GET forbid the use of body :(
-            headers: { 'Content-Type': 'application/json', },
-            body: JSON.stringify({ "name": url.searchParams.get("query") }),
-            credentials: 'include'
-        }).then(user => {
-            if (user.ok){
-                user.json().then(((user) => {
-                    document.getElementById("userResumeCount").innerHTML = Object.keys(user).length;
-                    document.getElementById("userResumeSearch").innerHTML = url.searchParams.get("query");
-                    Object.keys(user).forEach(function (key) {
-                        createUserResumeContainer(user[key]);
-                    })
+	url = new URL(window.location.href);
+	if (url.searchParams.get("query")) {
+		fetch('/api/user/search_by_username', {
+			method: 'POST', //GET forbid the use of body :(
+			headers: { 'Content-Type': 'application/json', },
+			body: JSON.stringify({ "name": url.searchParams.get("query") }),
+			credentials: 'include'
+		}).then(user => {
+			if (user.ok){
+				user.json().then(((user) => {
+					document.querySelector("#userResumeCountContainer").style.setProperty("display", "block")
+					document.getElementById("userResumeCount").innerHTML = Object.keys(user).length;
+					document.getElementById("userResumeSearch").innerHTML = url.searchParams.get("query");
+					Object.keys(user).forEach(function (key) {
+						createUserResumeContainer(user[key]);
+					})
+					userResume = document.querySelectorAll(".userResume");
+					for (i = 0; i<  userResume.length; i++){
+						userResume[i].addEventListener("click", (e) => {
+							var username = e.target.closest(".userResume").id;
+							myPushState(`https://${hostname.host}/user/${username}`);
+						})
+						userResume[i].addEventListener("keydown", (e) => {
+							if (e.key == "Enter"){
+								var username = e.target.closest(".userResume").id;
+								myPushState(`https://${hostname.host}/user/${username}`);
+							}
+						})
+					}
+				}))
+			}
+			else{
+				if (user.status == 401){
+					popUpError("how dare you >:("); //TODO change popup message
+					myReplaceState(`https://${hostname.host}/login`);
+				}
+				else
+					myReplaceState(`https://${hostname.host}/home`);
+			}
+		})
+	}
+	else{
+		var inputSearch = document.querySelector("#inputSearch");
+		document.querySelector("#searchBtn").addEventListener("click", function(){
+			myPushState(`https://${hostname.host}/search?query=${inputSearch.value}`);
+		});
+		inputSearch.onkeydown = function(e){
+			if (e.key == "Enter")
+				myPushState(`https://${hostname.host}/search?query=${inputSearch.value}`);
+		};
 
-                    userResume = document.querySelectorAll(".userResume");
-                    for (i = 0; i<  userResume.length; i++){
-                        userResume[i].addEventListener("click", (e) => {
-                            var username = e.target.closest(".userResume").id;
-                            myPushState(`https://${hostname.host}/user/${username}`);
-                        })
-                        userResume[i].addEventListener("keydown", (e) => {
-                            if (e.key == "Enter"){
-                                var username = e.target.closest(".userResume").id;
-                                myPushState(`https://${hostname.host}/user/${username}`);
-                            }
-                        })
-                    }
-                }))
-            }
-            else{
-                if (user.status == 401){
-                    popUpError("how dare you >:("); //TODO change popup message
-                    myReplaceState(`https://${hostname.host}/login`);
-                }
-                else
-                   myReplaceState(`https://${hostname.host}/home`);
-            }
-        })
-    }
-    else{
-        myReplaceState(`https://${hostname.host}/home`);
-		popUpError("Can't search empty query");
-    }
-
+		document.querySelector("#searchBtn").onkeydown = function(e){
+			if (e.key == "Enter")
+				myPushState(`https://${hostname.host}/search?query=${inputSearch.value}`);
+		};
+	}
 }
 
 async function updateSearchAriaLabel(key, content){
-    document.querySelectorAll(key).forEach(function (elem) {
-        elem.setAttribute("aria-label", `${elem.id} ${content}`);
+	document.querySelectorAll(key).forEach(function (elem) {
+		elem.setAttribute("aria-label", `${elem.id} ${content}`);
 	})
 }
 
