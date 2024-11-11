eca715e53dd9d4ee945a40dff02e00f522c96791
Eguelin <eguelin@student.42lyon.fr>
2024-11-11 17:40:11 +0100

    [feat] Add save GameAI

--- src/django/app/game/models.py
+++ src/django/app/game/models.py
@@ -49,13 +49,12 @@ class MatchManager(models.Manager):
 		player_two.profile.matches.add(match)
 		return match
 
-	def addMatch(self, playerOne : Player, playerTwo : Player, matchType : str):
+	def addMatch(self, playerOne : Player, playerTwo : Player):
 		match = self.create(player_one=playerOne.user, player_two=playerTwo.user)
-		match.type = matchType
 		match.player_one_pts = playerOne.score if playerOne.socket else 0
-		match.player_two_pts = playerTwo.score if playerTwo.socket else 0
+		match.player_two_pts = playerTwo.score if playerTwo.user.username == "AI" or playerTwo.socket else 0
 		match.date = datetime.datetime.now()
-		match.winner = playerOne.user if playerOne.score > playerTwo.score or not playerTwo.socket else playerTwo.user
+		match.winner = playerOne.user if playerOne.score > playerTwo.score or (playerTwo.user.username != "AI" and not playerTwo.socket) else playerTwo.user
 		match.save()
 		playerOne.user.profile.matches.add(match)
 		playerTwo.user.profile.matches.add(match)
@@ -64,7 +63,6 @@ class MatchManager(models.Manager):
 		super().save()
 
 class Match(models.Model):
-	type = models.CharField(max_length=10, default="random")
 	player_one = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="first_player")
 	player_two = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="second_player")
 	date = models.DateField(auto_now=False, auto_now_add=True)
