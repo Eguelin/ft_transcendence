c14e3841f7fc77b4459651a73ca79406652ae10f
Eguelin <eguelin@student.42lyon.fr>
2024-11-22 18:00:17 +0100

    refactor: improve send_friend_request logic with enhanced error handling and validation

--- src/django/app/friendship/views.py
+++ src/django/app/friendship/views.py
@@ -1,40 +1,47 @@
-from django.contrib.auth import login, authenticate, logout
-from django.db import DatabaseError, IntegrityError
 from django.contrib.auth.models import User
 from django.http import JsonResponse
-import json, os, requests, base64, random, string
-import login.models as customModels
+import json
 
 def send_friend_request(request):
 	if request.method != 'POST':
 		return JsonResponse({'message': 'Invalid request'}, status=400)
-	if request.user.is_authenticated:
+	if not request.user.is_authenticated:
+		return JsonResponse({'message': 'User not authenticated'}, status=400)
+
+	try:
 		data = json.loads(request.body)
 		username = data['username']
-		if (username == request.user.username):
-			return JsonResponse({'message': 'Can\'t send friend request to yourself'})
-		try:
-			new_friend = User.objects.get(username=username)
-			user = request.user
-			if (new_friend.profile.blocked_users.filter(pk=user.pk)).exists():
-				return JsonResponse({'message': 'User blocked you'})
-			if (user.profile.friends_request.filter(pk=new_friend.pk)).exists():
-				new_friend.profile.friends.add(user)
-				new_friend.save()
-				user.profile.friends_request.remove(new_friend)
-				user.profile.friends.add(new_friend)
-				user.save()
-				return JsonResponse({'message': 'Succesfully added friend'})
-			elif not ((new_friend.profile.friends_request.filter(pk=user.pk)).exists()):
-				new_friend.profile.friends_request.add(user)
-				new_friend.save()
-				return JsonResponse({'message': 'Request succesfully sent'})
-			else :
-				return JsonResponse({'message': 'Request already sent'})
-		except:
-			return JsonResponse({'message': 'Can\'t find user'}, status=400)
-	else:
-		return JsonResponse({'username': None}, status=400)
+	except json.JSONDecodeError:
+		return JsonResponse({'message':  "Invalid JSON"}, status=400)
+	except KeyError:
+		return JsonResponse({'message':  "Missing Data"}, status=400)
+
+	if not username or not isinstance(username, str):
+		return JsonResponse({'message': 'Missing username'}, status=400)
+
+	if (username == request.user.username):
+		return JsonResponse({'message': 'Can\'t send friend request to yourself'}, status=400)
+
+	try:
+		new_friend = User.objects.get(username=username)
+		user = request.user
+		if (new_friend.profile.blocked_users.filter(pk=user.pk)).exists():
+			return JsonResponse({'message': 'User blocked you'})
+		if (user.profile.friends_request.filter(pk=new_friend.pk)).exists():
+			new_friend.profile.friends.add(user)
+			new_friend.save()
+			user.profile.friends_request.remove(new_friend)
+			user.profile.friends.add(new_friend)
+			user.save()
+			return JsonResponse({'message': 'Succesfully added friend'})
+		elif not ((new_friend.profile.friends_request.filter(pk=user.pk)).exists()):
+			new_friend.profile.friends_request.add(user)
+			new_friend.save()
+			return JsonResponse({'message': 'Request succesfully sent'})
+		else :
+			return JsonResponse({'message': 'Request already sent'})
+	except User.DoesNotExist:
+		return JsonResponse({'message': 'Can\'t find user'}, status=404)
 
 def accept_friend_request(request):
 	if request.method != 'POST':
