fbfbdfd9bf05d75b0057e03ee7221d5974602527
Eguelin <eguelin@student.42lyon.fr>
2024-11-22 18:07:49 +0100

    refactor: improve remove_friend logic with enhanced authentication checks and error handling

--- src/django/app/friendship/views.py
+++ src/django/app/friendship/views.py
@@ -104,21 +104,32 @@ def reject_friend_request(request):
 def remove_friend(request):
 	if request.method != 'POST':
 		return JsonResponse({'message': 'Invalid request'}, status=400)
-	if request.user.is_authenticated:
+	if not request.user.is_authenticated:
+		return JsonResponse({'message': 'User not authenticated'}, status=400)
+	try:
 		data = json.loads(request.body)
 		username = data['username']
-		try:
-			friend = User.objects.get(username=username)
-			user = request.user
-			friend.profile.friends.remove(user)
-			friend.save()
-			user.profile.friends.remove(friend)
-			user.save()
-			return JsonResponse({'message': 'Succesfully added friend'})
-		except:
-			return JsonResponse({'message': 'Can\'t find user'}, status=400)
-	else:
-		return JsonResponse({'username': None}, status=400)
+	except json.JSONDecodeError:
+		return JsonResponse({'message':  "Invalid JSON"}, status=400)
+	except KeyError:
+		return JsonResponse({'message':  "Missing Data"}, status=400)
+
+	if not username or not isinstance(username, str):
+		return JsonResponse({'message': 'Missing username'}, status=400)
+
+	if (username == request.user.username):
+		return JsonResponse({'message': 'Can\'t remove friend from yourself'}, status=400)
+
+	try:
+		friend = User.objects.get(username=username)
+		user = request.user
+		friend.profile.friends.remove(user)
+		friend.save()
+		user.profile.friends.remove(friend)
+		user.save()
+		return JsonResponse({'message': 'Succesfully added friend'})
+	except:
+		return JsonResponse({'message': 'Can\'t find user'}, status=404)
 
 def block_friend(request):
 	if request.method != 'POST':
