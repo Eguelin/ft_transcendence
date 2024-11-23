cfb4ab388be32db68587c48a0d7b81ca4021835b
llevasse <elise.levasse@gmail.com>
2024-11-23 16:44:19 +0100

    [front] tweaked some stuff on dashboard page

--- src/nginx/html/scripts/dashboard.js
+++ src/nginx/html/scripts/dashboard.js
@@ -630,8 +630,7 @@ function loadUserDashboard(startDate, endDate){
     var splitPath = window.location.href.split('/');
 	(async () => {
 		dashboard = await new Dashboard(startDate, endDate, splitPath[4], client.username);
-		unsetLoader();
-		displayCharts();
+		setTimeout(function(){unsetLoader();displayCharts()}, 500);
 
 		matchObj = dashboard.matches[Object.keys(dashboard.matches)[Object.keys(dashboard.matches).length - 1]] // get matches object of highest date
 
