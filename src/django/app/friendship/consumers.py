429c8f49dfbae53729d3977f81912b2ad5857e18
naterrie <nathanterrier38580@gmail.com>
2024-12-03 09:45:17 +0100

    update on friend consumer, using only receiver, blocking multiple div friend request from one personne

--- src/django/app/friendship/consumers.py
+++ src/django/app/friendship/consumers.py
@@ -28,50 +28,13 @@ class friend(AsyncWebsocketConsumer):
 				self.channel_name
 			)
 
-	async def receive(self, text_data):
-		data = json.loads(text_data)
-
-		if not isinstance(data, dict):
-			await self.send(text_data=json.dumps({
-				'type': 'error',
-				'message': 'Invalid data'
-			}))
-			return
-		type = data.get('type')
-
-		if type == 'send_friend_request':
-			await self.send_friend_request_notif(data.get('target_user_id'), data.get('sender_username'))
-
-	async def send_friend_request_notif(self, user_id, sender_name):
-		try:
-				if not isinstance(user_id, int) or user_id <= 0:
-						raise ValueError("user id is invalid")
-
-				if not sender_name or not isinstance(sender_name, str):
-						raise ValueError("username is invalid")
-
-				channel_layer = get_channel_layer()
-
-				await channel_layer.group_send(
-						f"user_{user_id}",
-						{
-								'type': 'send_friend_request',
-								'new_request': True,
-								'sender_name': sender_name
-						}
-				)
-		except ValueError as e:
-			await self.send(text_data=json.dumps({
-			'type': 'error',
-			'message': str(e)
-		}))
-
+	async def receive(self):
+		pass
 
 	async def send_friend_request(self, event):
 		await self.send(text_data=json.dumps({
 			'new_request': event['new_request'],
 			'sender_name' : event['sender_name']
-
 		}))
 
 	async def friend_status_update(self, event):
@@ -86,6 +49,22 @@ class friend(AsyncWebsocketConsumer):
 		'username': event['message']['username'],
 	}))
 
+@receiver(m2m_changed, sender=Profile.friends_request.through)
+def notify_friend_request_changed(sender, instance, action, pk_set, **kwargs):
+	channel_layer = get_channel_layer()
+	user = instance.user
+
+	if action == "post_add":
+		new_requests = Profile.objects.filter(user__id__in=pk_set)
+		for new_request in new_requests:
+			async_to_sync(channel_layer.group_send)(
+				f"user_{user.id}",
+				{
+					"type": "send_friend_request",
+					"new_request": True,
+					"sender_name": new_request.user.username,
+				}
+			)
 
 @receiver(m2m_changed, sender=Profile.friends.through)
 def notify_friend_removed(sender, instance, action, pk_set, **kwargs):
